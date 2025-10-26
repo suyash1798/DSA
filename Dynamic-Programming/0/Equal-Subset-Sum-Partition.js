9d8df14f4a8c46e8abe084abe569e0fdb94e7873
Suyash Tiwari <suyashtiwari1798@gmail.com>
2025-10-26 06:16:41 +0000

    equal subset sum partition

--- Dynamic-Programming/0/Equal-Subset-Sum-Partition.js
+++ Dynamic-Programming/0/Equal-Subset-Sum-Partition.js
@@ -14,32 +14,32 @@ class Solution {
 
   canPartition(num) {
     let sum = num.reduce((total, n) => total + n, 0);
-    let partitionSum = sum/2;
+    let partitionSum = sum / 2;
 
-    if(sum%2 !== 0){
+    if (sum % 2 !== 0) {
       return false;
     }
 
-    let memo = new Array(num.length).fill(0).map(()=> []);
+    let memo = new Array(num.length).fill(0).map(() => []);
 
     return this.canPartitionUtil(num, 0, partitionSum, memo);
   }
 
-  canPartitionUtil(num, index, partitionSum, memo){
-    if(partitionSum === 0){
+  canPartitionUtil(num, index, partitionSum, memo) {
+    if (partitionSum === 0) {
       return true;
     }
 
-    if(partitionSum < 0 || index === num.length){
+    if (partitionSum < 0 || index === num.length) {
       return false;
     }
 
-    if(memo[index][partitionSum] !== undefined){
+    if (memo[index][partitionSum] !== undefined) {
       return memo[index][partitionSum];
     }
 
-    let include = this.canPartitionUtil(num, index+1, partitionSum-num[index], memo);
-    let exlude = this.canPartitionUtil(num, index+1, partitionSum, memo);
+    let include = this.canPartitionUtil(num, index + 1, partitionSum - num[index], memo);
+    let exlude = this.canPartitionUtil(num, index + 1, partitionSum, memo);
 
     return memo[index][partitionSum] = (include || exlude);
   }
