86644bcfc61dfc171e36c95f315a45dffd484489
Suyash Tiwari <suyashtiwari1798@gmail.com>
2025-10-31 20:29:36 +0000

    Deepest Leaves Sum

---
+++ Tree/Deepest-Leaves-Sum.js
@@ -0,0 +1,50 @@
+/*
+
+Given the root of a binary tree, return the sum of values of its deepest leaves.
+
+Example 1:
+
+Input: root = [1,2,3,4,5,null,6,7,null,null,null,null,8]
+Output: 15
+
+Example 2:
+
+Input: root = [6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]
+Output: 19
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var deepestLeavesSum = function (root) {
+    let queue = [root], sum = 0;
+
+    while (queue.length) {
+        let nextQueue = [];
+        sum = 0;
+
+        for (let index = 0; index < queue.length; index++) {
+            let node = queue[index];
+
+            sum += node.val;
+
+            node.left && nextQueue.push(node.left);
+            node.right && nextQueue.push(node.right);
+        }
+
+        queue = nextQueue;
+    }
+
+    return sum;
+};
