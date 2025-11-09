9c84d18e986c30c358047085add92092afe769ee
Suyash Tiwari <suyashtiwari1798@gmail.com>
2025-11-09 07:47:52 +0000

    Cheapest Flights Within K Stops

---
+++ Graph/Cheapest-Flights-Within-K-Stops.js
@@ -0,0 +1,86 @@
+/*
+
+There are n cities connected by some number of flights. You are given an array flights where flights[i] = [fromi, toi, pricei] indicates that there is a flight from city fromi to city toi with cost pricei.
+
+You are also given three integers src, dst, and k, return the cheapest price from src to dst with at most k stops. If there is no such route, return -1.
+
+ 
+
+Example 1:
+
+
+Input: n = 4, flights = [[0,1,100],[1,2,100],[2,0,100],[1,3,600],[2,3,200]], src = 0, dst = 3, k = 1
+Output: 700
+Explanation:
+The graph is shown above.
+The optimal path with at most 1 stop from city 0 to 3 is marked in red and has cost 100 + 600 = 700.
+Note that the path through cities [0,1,2,3] is cheaper but is invalid because it uses 2 stops.
+Example 2:
+
+
+Input: n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, k = 1
+Output: 200
+Explanation:
+The graph is shown above.
+The optimal path with at most 1 stop from city 0 to 2 is marked in red and has cost 100 + 100 = 200.
+Example 3:
+
+
+Input: n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, k = 0
+Output: 500
+Explanation:
+The graph is shown above.
+The optimal path with no stops from city 0 to 2 is marked in red and has cost 500.
+ 
+
+Constraints:
+
+2 <= n <= 100
+0 <= flights.length <= (n * (n - 1) / 2)
+flights[i].length == 3
+0 <= fromi, toi < n
+fromi != toi
+1 <= pricei <= 104
+There will not be any multiple flights between two cities.
+0 <= src, dst, k < n
+src != dst
+
+*/
+
+
+/**
+ * @param {number} n
+ * @param {number[][]} flights
+ * @param {number} src
+ * @param {number} dst
+ * @param {number} k
+ * @return {number}
+ */
+var findCheapestPrice = function (n, flights, src, dst, k) {
+    let graph = {}, stops = new Array(n).fill(Infinity);
+
+    for (let flight of flights) {
+        let [from, to, cost] = flight;
+        if (!graph[from]) graph[from] = [];
+        graph[from].push([to, cost]);
+    }
+
+    let heap = new MinPriorityQueue((a) => a[1]);
+
+    heap.enqueue([src, 0, -1]); // node, cost, stops
+    stops[src] = 0;
+
+    while (heap.size()) {
+        let [s, c, st] = heap.dequeue();
+        
+        if (s === dst) return c;
+        stops[s] = st;
+
+        for (let [n, nc] of (graph[s] || [])) {
+            if (st + 1 > k || stops[n] < st + 1) continue;
+            heap.enqueue([n, c + nc, st + 1]);
+        }
+    }
+
+    return -1;
+};
