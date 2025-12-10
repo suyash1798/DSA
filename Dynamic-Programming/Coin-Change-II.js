5ea92f7c89f751124c9f149494bbf3d9ac02088d
Suyash Tiwari <suyashtiwari1798@gmail.com>
2025-12-10 17:20:35 +0000

    last-stone-weight-ii

--- Dynamic-Programming/Coin-Change-II.js
+++ Dynamic-Programming/Coin-Change-II.js
@@ -60,3 +60,30 @@ var change = function (amount, coins) {
 
     return solve(amount, 0);
 };
+
+
+
+/**.   Bottom-up approach */
+
+
+
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function (amount, coins) {
+    let dp = new Array(amount + 1).fill(0);
+
+    dp[0] = 1;
+
+    for (let coin of coins) {
+        for (let i = 1; i <= amount; i++) {
+            if (i - coin >= 0) {
+                dp[i] += dp[i - coin];
+            }
+        }
+    }
+
+    return dp[amount];
+};
