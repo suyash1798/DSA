2a982e76e1e738b6c2077cff414b570dbd863e0f
Suyash Tiwari <suyashtiwari1798@gmail.com>
2026-01-13 03:20:04 +0000

    Distinct Subsequence bottom up

--- Dynamic-Programming/Distinct-Subsequences.js
+++ Dynamic-Programming/Distinct-Subsequences.js
@@ -47,23 +47,46 @@ s and t consist of English letters.
  * @return {number}
  */
 var numDistinct = function (s, t) {
-    let dp = new Array(s.length).fill(0).map(() => new Array(t.length).fill(-1));
+    let memo = new Array(s.length).fill(0).map(() => new Array(t.length).fill(-1));
+    function dfs(si, ti) {
+        if (ti === t.length) return 1;
 
-    function dfs(index1, index2) {
-        if (index2 === t.length) return 1;
-        if (index1 === s.length) return 0;
+        if (si === s.length) return 0;
 
-        if(dp[index1][index2] !== -1) return dp[index1][index2];
+        if (memo[si][ti] !== -1) return memo[si][ti];
 
         let count = 0;
 
-        if (s[index1] === t[index2]) {
-            count += dfs(index1 + 1, index2 + 1);
-        }
-        count += dfs(index1 + 1, index2);
+        if (s[si] === t[ti]) count += dfs(si + 1, ti + 1);
 
-        return dp[index1][index2] = count;
+        count += dfs(si + 1, ti);
+
+        return memo[si][ti] = count;
     }
 
     return dfs(0, 0);
+};
+
+
+/// Top Down
+
+
+var numDistinct = function (s, t) {
+    let dp = new Array(s.length + 1).fill(0).map(() => new Array(t.length + 1).fill(0));
+
+    for (let i = 0; i < s.length + 1; i++) {
+        dp[i][t.length] = 1;
+    }
+
+    for (let i = s.length - 1; i >= 0; i--) {
+        for (let j = t.length - 1; j >= 0; j--) {
+            dp[i][j] += dp[i + 1][j];
+
+            if (s[i] === t[j]) {
+                dp[i][j] += dp[i + 1][j + 1];
+            }
+        }
+    }
+
+    return dp[0][0];
 };
