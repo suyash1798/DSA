59b11d845dab42279365bd90f7e5c167202e3f76
Suyash Tiwari <suyashtiwari1798@gmail.com>
2026-01-17 17:56:42 +0000

    Kth Smallest Element in a BST

---
+++ Tree/Kth-Smallest-Element-in-a-BST.js
@@ -0,0 +1,62 @@
+/*
+
+Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,1,4,null,2], k = 1
+Output: 1
+Example 2:
+
+
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+Output: 3
+ 
+
+Constraints:
+
+The number of nodes in the tree is n.
+1 <= k <= n <= 104
+0 <= Node.val <= 104
+
+
+*/
+
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function (root, k) {
+    let kth = 0, count = 0;
+
+    function dfs(root) {
+        if (!root) return;
+
+        let left = dfs(root.left);
+
+        count++;
+        if (count === k) {
+            kth = root.val;
+        }
+
+        dfs(root.right);
+    }
+
+    dfs(root);
+
+    return kth;
+};
