cd33d0c6492e2b8d767964c053c1fdd503345f0d
Suyash Tiwari <suyashtiwari1798@gmail.com>
2026-02-11 14:54:14 +0000

    Minimum Total Distance Traveled

---
+++ Dynamic-Programming/Minimum-Total-Distance-Traveled.js
@@ -0,0 +1,163 @@
+/*
+
+There are some robots and factories on the X-axis. You are given an integer array robot where robot[i] is the position of the ith robot. You are also given a 2D integer array factory where factory[j] = [positionj, limitj] indicates that positionj is the position of the jth factory and that the jth factory can repair at most limitj robots.
+
+The positions of each robot are unique. The positions of each factory are also unique. Note that a robot can be in the same position as a factory initially.
+
+All the robots are initially broken; they keep moving in one direction. The direction could be the negative or the positive direction of the X-axis. When a robot reaches a factory that did not reach its limit, the factory repairs the robot, and it stops moving.
+
+At any moment, you can set the initial direction of moving for some robot. Your target is to minimize the total distance traveled by all the robots.
+
+Return the minimum total distance traveled by all the robots. The test cases are generated such that all the robots can be repaired.
+
+Note that
+
+All robots move at the same speed.
+If two robots move in the same direction, they will never collide.
+If two robots move in opposite directions and they meet at some point, they do not collide. They cross each other.
+If a robot passes by a factory that reached its limits, it crosses it as if it does not exist.
+If the robot moved from a position x to a position y, the distance it moved is |y - x|.
+ 
+
+Example 1:
+
+
+Input: robot = [0,4,6], factory = [[2,2],[6,2]]
+Output: 4
+Explanation: As shown in the figure:
+- The first robot at position 0 moves in the positive direction. It will be repaired at the first factory.
+- The second robot at position 4 moves in the negative direction. It will be repaired at the first factory.
+- The third robot at position 6 will be repaired at the second factory. It does not need to move.
+The limit of the first factory is 2, and it fixed 2 robots.
+The limit of the second factory is 2, and it fixed 1 robot.
+The total distance is |2 - 0| + |2 - 4| + |6 - 6| = 4. It can be shown that we cannot achieve a better total distance than 4.
+Example 2:
+
+
+Input: robot = [1,-1], factory = [[-2,1],[2,1]]
+Output: 2
+Explanation: As shown in the figure:
+- The first robot at position 1 moves in the positive direction. It will be repaired at the second factory.
+- The second robot at position -1 moves in the negative direction. It will be repaired at the first factory.
+The limit of the first factory is 1, and it fixed 1 robot.
+The limit of the second factory is 1, and it fixed 1 robot.
+The total distance is |2 - 1| + |(-2) - (-1)| = 2. It can be shown that we cannot achieve a better total distance than 2.
+ 
+
+Constraints:
+
+1 <= robot.length, factory.length <= 100
+factory[j].length == 2
+-109 <= robot[i], positionj <= 109
+0 <= limitj <= robot.length
+The input will be generated such that it is always possible to repair every robot.
+
+*/
+
+
+
+/**
+ * @param {number[]} robot
+ * @param {number[][]} factory
+ * @return {number}
+ */
+var minimumTotalDistance = function (robot, factory) {
+    robot.sort((a, b) => a - b);
+    factory.sort((a, b) => a[0] - b[0]);
+
+    let dp = new Array(robot.length).fill(0).map(() => new Array(factory.length).fill(0).map(() => new Array(101).fill(-1)));
+
+    function dfs(ri, fi, c) {
+        if (ri === robot.length) return 0;
+
+        if (fi === factory.length) return Infinity;
+
+        if (dp[ri][fi][c] !== -1) return dp[ri][fi][c];
+
+        let min = Infinity;
+
+        if (fi !== factory.length - 1) {
+            min = Math.min(min, dfs(ri, fi + 1, factory[fi + 1][1]));
+        }
+
+        if (c !== 0) {
+            min = Math.min(min, Math.abs(robot[ri] - factory[fi][0]) + dfs(ri + 1, fi, c - 1));
+        }
+
+        return dp[ri][fi][c] = min;
+    }
+
+    return dfs(0, 0, factory[0][1]);
+};
+
+
+/**
+ * @param {number[]} robot
+ * @param {number[][]} factory
+ * @return {number}
+ */
+var minimumTotalDistance = function (robot, factory) {
+    robot.sort((a, b) => a - b);
+    factory.sort((a, b) => a[0] - b[0]);
+
+    let memo = new Array(robot.length).fill(0).map(() => new Array(factory.length).fill(-1));
+
+    function dfs(ri, fi) {
+        if (ri === robot.length) return 0;
+
+        if (fi === factory.length) return Infinity;
+
+        if (memo[ri][fi] !== -1) return memo[ri][fi];
+
+        let min = dfs(ri, fi + 1);
+        let dist = 0;
+
+        for (let c = 0; c < factory[fi][1]; c++) {
+            if (ri + c < robot.length) {
+                dist += Math.abs(robot[ri + c] - factory[fi][0]);
+
+                min = Math.min(min, dist + dfs(ri + c + 1, fi + 1));
+            }
+        }
+
+        return memo[ri][fi] = min;
+    }
+
+    return dfs(0, 0);
+};
+
+
+
+
+
+
+var minimumTotalDistance = function (robot, factory) {
+    robot.sort((a, b) => a - b);
+    factory.sort((a, b) => a[0] - b[0]);
+
+    let dp = new Array(robot.length+1).fill(0).map(() => new Array(factory.length+1).fill(Infinity));
+
+    for (let i = 0; i <= factory.length; i++) {
+        dp[robot.length][i] = 0;
+    }
+
+    for (let i = robot.length - 1; i >= 0; i--) {
+        for (let j = factory.length - 1; j >= 0; j--) {
+            let min = dp[i][j + 1];
+            let dist = 0;
+
+            for (let c = 0; c < factory[j][1]; c++) {
+                if (i + c < robot.length) {
+                    dist += Math.abs(robot[i + c] - factory[j][0]);
+
+                    min = Math.min(min, dist + dp[i + c + 1][j + 1]);
+                }
+            }
+
+            dp[i][j] = min;
+        }
+    }
+
+    return dp[0][0];
+};
+
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 🚀 DSA Patterns - JavaScript
 
-**Total Problems Solved: 542** 🎉
+**Total Problems Solved: 543** 🎉
 
 ## 📖 Problems by Topic
 
@@ -114,7 +114,7 @@
 17. [Seat Reservation Manager](./Design/Seat-Reservation-Manager.js)
 18. [Maximum Frequency Stack](./Design/Maximum-Frequency-Stack.ts)
 
-### 💡 Dynamic Programming (75)
+### 💡 Dynamic Programming (76)
 
 1. [0 1 KnapSack](./Dynamic-Programming/0-1-KnapSack.js)
 2. [Coin Change II](./Dynamic-Programming/Coin-Change-II.js)
@@ -191,6 +191,7 @@
 73. [Minimum Number of Operations to Make X and Y Equal](./Dynamic-Programming/Minimum-Number-of-Operations-to-Make-X-and-Y-Equal.js)
 74. [Number of Ways of Cutting a Pizza](./Dynamic-Programming/Number-of-Ways-of-Cutting-a-Pizza.js)
 75. [Selling Pieces of Wood](./Dynamic-Programming/Selling-Pieces-of-Wood.js)
+76. [Minimum Total Distance Traveled](./Dynamic-Programming/Minimum-Total-Distance-Traveled.js)
 
 ### 🧭 Graph (44)
 
