34d8e6d53a8c33f15804808bff17fa8182fb6a24
Suyash Tiwari <suyashtiwari1798@gmail.com>
2026-04-17 18:26:03 +0000

    Integers With Multiple Sum of Two Cubes

---
+++ Dynamic-Programming/Minimum-Increase-to-Maximize-Special-Indices.py
@@ -0,0 +1,65 @@
+# You are given an integer n.
+
+# An integer x is considered good if there exist at least two distinct pairs (a, b) such that:
+
+# a and b are positive integers.
+# a <= b
+# x = a3 + b3
+# Return an array containing all good integers less than or equal to n, sorted in ascending order.
+
+ 
+
+# Example 1:
+
+# Input: n = 4104
+
+# Output: [1729,4104]
+
+# Explanation:
+
+# Among integers less than or equal to 4104, the good integers are:
+
+# 1729: 13 + 123 = 1729 and 93 + 103 = 1729.
+# 4104: 23 + 163 = 4104 and 93 + 153 = 4104.
+# Thus, the answer is [1729, 4104].
+
+# Example 2:
+
+# Input: n = 578
+
+# Output: []
+
+# Explanation:
+
+# There are no good integers less than or equal to 578, so the answer is an empty array.
+
+ 
+
+# Constraints:
+
+# 1 <= n <= 109
+ 
+
+
+
+
+class Solution:
+    def findGoodIntegers(self, n: int) -> list[int]:
+        upper = int(n ** (1 / 3)) + 1
+        last = set()
+        output = set()
+
+        for i in range(upper + 1):
+            for j in range(i, upper + 1):
+                x = i**3 + j**3
+
+                if x in last and x <= n:
+                    output.add(x)
+
+                last.add(x)
+
+        output = list(output)
+        output.sort()
+
+        return output
+
