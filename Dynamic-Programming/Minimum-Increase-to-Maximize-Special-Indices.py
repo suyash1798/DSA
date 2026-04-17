8f77e2a0d4150f574891213d1594c3b0bad3b349
Suyash Tiwari <suyashtiwari1798@gmail.com>
2026-04-17 18:27:27 +0000

    Integers With Multiple Sum of Two Cubes

--- Dynamic-Programming/Minimum-Increase-to-Maximize-Special-Indices.py
+++ Dynamic-Programming/Minimum-Increase-to-Maximize-Special-Indices.py
@@ -1,65 +1,86 @@
-# You are given an integer n.
+# You are given an integer array nums of length n.
 
-# An integer x is considered good if there exist at least two distinct pairs (a, b) such that:
+# An index i (0 < i < n - 1) is special if nums[i] > nums[i - 1] and nums[i] > nums[i + 1].
 
-# a and b are positive integers.
-# a <= b
-# x = a3 + b3
-# Return an array containing all good integers less than or equal to n, sorted in ascending order.
+# You may perform operations where you choose any index i and increase nums[i] by 1.
+
+# Your goal is to:
+
+# Maximize the number of special indices.
+# Minimize the total number of operations required to achieve that maximum.
+# Return an integer denoting the minimum total number of operations required.
 
  
 
 # Example 1:
 
-# Input: n = 4104
+# Input: nums = [1,2,2]
 
-# Output: [1729,4104]
+# Output: 1
 
-# Explanation:
+# Explanation:​​​​​​​
 
-# Among integers less than or equal to 4104, the good integers are:
-
-# 1729: 13 + 123 = 1729 and 93 + 103 = 1729.
-# 4104: 23 + 163 = 4104 and 93 + 153 = 4104.
-# Thus, the answer is [1729, 4104].
-
+# Start with nums = [1, 2, 2].
+# Increase nums[1] by 1, array becomes [1, 3, 2].
+# The final array is [1, 3, 2] has 1 special index, which is the maximum achievable.
+# It is impossible to achieve this number of special indices with fewer operations. Thus, the answer is 1.
 # Example 2:
 
-# Input: n = 578
+# Input: nums = [2,1,1,3]
 
-# Output: []
+# Output: 2
 
-# Explanation:
+# Explanation:​​​​​​​
 
-# There are no good integers less than or equal to 578, so the answer is an empty array.
+# Start with nums = [2, 1, 1, 3].
+# Perform 2 operations at index 1, array becomes [2, 3, 1, 3].
+# The final array is [2, 3, 1, 3] has 1 special index, which is the maximum achievable. Thus, the answer is 2.
+# Example 3:
 
+# Input: nums = [5,2,1,4,3]
+
+# Output: 4
+
+# Explanation:​​​​​​​​​​​​​​​​​​​​​
+
+# Start with nums = [5, 2, 1, 4, 3].
+# Perform 4 operations at index 1, array becomes [5, 6, 1, 4, 3].
+# The final array is [5, 6, 1, 4, 3] has 2 special indices, which is the maximum achievable. Thus, the answer is 4.​​​​​​​
  
 
 # Constraints:
 
-# 1 <= n <= 109
+# 3 <= n <= 105
+# 1 <= nums[i] <= 109
  
 
 
 
-
 class Solution:
-    def findGoodIntegers(self, n: int) -> list[int]:
-        upper = int(n ** (1 / 3)) + 1
-        last = set()
-        output = set()
+    def minIncrease(self, nums: List[int]) -> int:
 
-        for i in range(upper + 1):
-            for j in range(i, upper + 1):
-                x = i**3 + j**3
+        @cache
+        def dfs(index):
+            if index >= (len(nums) - 1):
+                return [0, 0]
 
-                if x in last and x <= n:
-                    output.add(x)
+            inc = max(0, max(nums[index-1], nums[index+1]) - nums[index] + 1)
 
-                last.add(x)
+            inc1, c1 = dfs(index+2)
+            inc2, c2 = dfs(index+1)
 
-        output = list(output)
-        output.sort()
+            inc1 = inc1 + inc
+            c1 = c1 + 1
+            
+            finc, fc = inc1, c1
+            
+            if c2 > c1:
+                finc, fc = inc2, c2
+            elif c1 == c2:
+                finc = min(inc1, inc2)
 
-        return output
+            return [finc, fc]
 
+        inc, c = dfs(1)
+
+        return inc
