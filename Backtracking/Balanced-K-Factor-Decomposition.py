44c43440a997a9efc57d65723564f5abf7b8592d
Suyash Tiwari <suyashtiwari1798@gmail.com>
2026-05-01 18:56:21 +0000

    Balanced K-Factor Decomposition

---
+++ Backtracking/Balanced-K-Factor-Decomposition.py
@@ -0,0 +1,74 @@
+# Given two integers n and k, split the number n into exactly k positive integers such that the product of these integers is equal to n.
+
+# Return any one split in which the maximum difference between any two numbers is minimized. You may return the result in any order.
+
+ 
+
+# Example 1:
+
+# Input: n = 100, k = 2
+
+# Output: [10,10]
+
+# Explanation:
+
+# The split [10, 10] yields 10 * 10 = 100 and a max-min difference of 0, which is minimal.
+
+# Example 2:
+
+# Input: n = 44, k = 3
+
+# Output: [2,2,11]
+
+# Explanation:
+
+# Split [1, 1, 44] yields a difference of 43
+# Split [1, 2, 22] yields a difference of 21
+# Split [1, 4, 11] yields a difference of 10
+# Split [2, 2, 11] yields a difference of 9
+# Therefore, [2, 2, 11] is the optimal split with the smallest difference 9.
+
+ 
+
+# Constraints:
+
+# 4 <= n <= 105
+# 2 <= k <= 5
+# k is strictly less than the total number of positive divisors of n.
+
+
+
+class Solution:
+    def minDifference(self, n: int, k: int) -> List[int]:
+        split = []
+        final = []
+        diff = n
+
+        def dfs(num, count):
+            nonlocal diff
+            nonlocal final
+
+            if count == k-1:
+                split.append(num)
+                mx = max(split)
+                mn = min(split)
+
+                if (mx - mn) <= diff:
+                    diff = mx - mn
+                    final = split.copy()
+
+                split.pop()
+                return
+            
+            for i in range(1, num+1):
+                if num % i == 0:
+                    split.append(i)
+                    dfs(num // i , count+1)
+                    split.pop()
+        
+        dfs(n, 0)
+
+        return final
+
+                
+                
