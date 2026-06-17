0783ec23da5851a84895ae12e22e07dc22b020af
Suyash Tiwari <suyashtiwari1798@gmail.com>
2026-06-17 18:16:16 +0000

    Ways to Express an Integer as Sum of Powers

---
+++ Dynamic-Programming/Ways-to-Express-an-Integer-as-Sum-of-Powers.py
@@ -0,0 +1,56 @@
+# Given two positive integers n and x.
+
+# Return the number of ways n can be expressed as the sum of the xth power of unique positive integers, in other words, the number of sets of unique integers [n1, n2, ..., nk] where n = n1x + n2x + ... + nkx.
+
+# Since the result can be very large, return it modulo 109 + 7.
+
+# For example, if n = 160 and x = 3, one way to express n is n = 23 + 33 + 53.
+
+ 
+
+# Example 1:
+
+# Input: n = 10, x = 2
+# Output: 1
+# Explanation: We can express n as the following: n = 32 + 12 = 10.
+# It can be shown that it is the only way to express 10 as the sum of the 2nd power of unique integers.
+# Example 2:
+
+# Input: n = 4, x = 1
+# Output: 2
+# Explanation: We can express n in the following ways:
+# - n = 41 = 4.
+# - n = 31 + 11 = 4.
+ 
+
+# Constraints:
+
+# 1 <= n <= 300
+# 1 <= x <= 5
+
+
+class Solution:
+    def numberOfWays(self, n: int, x: int) -> int:
+        
+        limit = floor((n ** (1/x)) + 1)
+        mod = (10 ** 9) + 7
+
+        @cache
+        def dfs(i, target):
+            if target == 0:
+                return 1
+            
+            if target < 0 or i > limit:
+                return 0
+            
+            take = dfs(i+1, target - (i ** x))
+            skip = dfs(i+1, target)
+            
+            return (take + skip) % mod
+        
+        ans = dfs(1, n)
+
+        dfs.cache_clear()
+
+        return ans
+
