dbbdff8f7717ad17c59d2ffced7f2f5f2f4e7eca
Suyash Tiwari <suyashtiwari1798@gmail.com>
2026-07-16 19:53:24 +0000

    Alternating Groups II

---
+++ Sliding-Window/Alternating-Groups-II.py
@@ -0,0 +1,83 @@
+# There is a circle of red and blue tiles. You are given an array of integers colors and an integer k. The color of tile i is represented by colors[i]:
+
+# colors[i] == 0 means that tile i is red.
+# colors[i] == 1 means that tile i is blue.
+# An alternating group is every k contiguous tiles in the circle with alternating colors (each tile in the group except the first and last one has a different color from its left and right tiles).
+
+# Return the number of alternating groups.
+
+# Note that since colors represents a circle, the first and the last tiles are considered to be next to each other.
+
+ 
+
+# Example 1:
+
+# Input: colors = [0,1,0,1,0], k = 3
+
+# Output: 3
+
+# Explanation:
+
+
+
+# Alternating groups:
+
+
+
+# Example 2:
+
+# Input: colors = [0,1,0,0,1,0,1], k = 6
+
+# Output: 2
+
+# Explanation:
+
+
+
+# Alternating groups:
+
+
+
+# Example 3:
+
+# Input: colors = [1,1,0,1], k = 4
+
+# Output: 0
+
+# Explanation:
+
+
+
+ 
+
+# Constraints:
+
+# 3 <= colors.length <= 105
+# 0 <= colors[i] <= 1
+# 3 <= k <= colors.length
+
+
+
+
+class Solution:
+    def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
+        
+        left, right = 0, 1
+        l = len(colors)
+        count = 0
+
+        while left < l:
+        
+            if colors[right % l] == colors[(right - 1 + l)%l]:
+                left = right
+                right += 1
+                continue
+            
+            if (right - left + 1) == k:
+                count += 1
+                left += 1
+            
+            right += 1
+
+        
+        return count
